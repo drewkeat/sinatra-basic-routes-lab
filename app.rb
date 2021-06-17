db0aa18fbe41fff5fd238b590e42592dbefb020b
Drew Keat <thedrewkeat@gmail.com>
2021-06-17 16:47:37 -0600

    commit tree 8c4ac3c4be6ff92c4efa1b22fb398db9bbd58324

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Drew Keat"
+    end
+
+    get '/hometown' do
+        "My hometown is Littleton, CO"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is \"Friday\" by Rebecca Black"
+    end
 end
